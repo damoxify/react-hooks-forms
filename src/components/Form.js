0218fb7f6f0da5335cee1f61bd05b829d29d42ad
Adedamola Ajasa <azeemajasa@gmail.com>
2023-09-08 04:52:03 +0100

    Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,25 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+        <input
+        type="text"
+        value={firstName}
+        onChange={handleFirstNameChange} 
+      />
+      <input
+        type="text"
+        value={lastName}
+        onChange={handleLastNameChange} 
+      />
       <button type="submit">Submit</button>
     </form>
   );
